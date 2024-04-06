d112ae4a312dd182ccb67653be78ccba20fbff9d
maithianhtuyet05 <maithianhtuyet05012002@gmail.com>
2024-04-07 02:18:37 +0700

    change style

--- datn-fe/src/common/Header.js
+++ datn-fe/src/common/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../static/css/style.css";
 import logo from "../static/images/logo-sneaker1.jpg";
-import { NavLink, useHistory } from "react-router-dom";
+import {NavLink, useHistory} from "react-router-dom";
 import user_image from "../static/images/default.png";
 import Dropdown from "../admin/dropdown/Dropdown";
-import { toast } from "react-toastify";
+import {toast} from "react-toastify";
 
 const user_menu = [
   {
@@ -49,7 +49,7 @@ const Header = (props) => {
   const renderUserToggle = (user) => (
     <div className="topnav__right-user">
       <div className="topnav__right-user__image">
-        <img src={user.image} alt="" />
+        <img src={user.image} alt=""/>
       </div>
       <div className="topnav__right-user__name">{user.display_name}</div>
     </div>
@@ -78,130 +78,139 @@ const Header = (props) => {
     props.userHandler(null);
   };
 
+  var style = `
+
+`
   return (
-    <div className="mini-card">
-      {/* Navigation */}
-      <nav className="navbar navbar-expand-md col-12">
-        <div className="navbar-brand ml-1 col">
-          <img src={logo} width={50} height={50} alt="" />
-        </div>
-        <div className="collapse navbar-collapse col">
-          <ul className="navbar-nav mini-ul">
-            <li
-              className={
-                props.header === 1
-                  ? "nav-item mr-2  mini-item active"
-                  : "nav-item mr-2  mini-item"
-              }
-            >
-              <NavLink className="nav-link" to="/" exact>
-                Trang chủ
-              </NavLink>
-            </li>
-            <li
-              className={
-                props.header === 2
-                  ? "nav-item mr-2  mini-item active"
-                  : "nav-item mr-2  mini-item"
-              }
-            >
-              <NavLink className="nav-link" to="/store" exact>
-                Sản phẩm
-              </NavLink>
-            </li>
-            <li
-              className={
-                props.header === 3
-                  ? "nav-item mr-2  mini-item active"
-                  : "nav-item mr-2  mini-item"
-              }
-            >
-              <NavLink className="nav-link" to="/cart" exact>
-                Giỏ hàng
-              </NavLink>
-            </li>
-            {props.user && (
+    <>
+      <style>
+        {style}
+      </style>
+      <div className="mini-card">
+        {/* Navigation */}
+        <nav className="navbar navbar-expand-md col-12">
+          <div className="navbar-brand ml-1 col">
+            <img src={logo} width={50} height={50} alt=""/>
+          </div>
+          <div className="collapse navbar-collapse col">
+            <ul className="navbar-nav mini-ul">
               <li
                 className={
-                  props.header === 5
-                    ? "nav-item mr-2  mini-item active"
-                    : "nav-item mr-2  mini-item"
+                  props.header === 1
+                    ? "nav-item mr-2 active"
+                    : "nav-item mr-2"
                 }
               >
-                <NavLink className="nav-link" to="/order" exact>
-                  Đơn hàng
+                <NavLink className="nav-link" to="/" exact>
+                  Trang chủ
                 </NavLink>
               </li>
-            )}
-            <li
-              className={
-                props.header === 4
-                  ? "nav-item mr-2  mini-item active"
-                  : "nav-item mr-2  mini-item"
-              }
-            >
-              <NavLink className="nav-link" to="/blog" exact>
-                Chính sách
-              </NavLink>
-            </li>
-            {props.user && (
               <li
-              className={
-                props.header === 6
-                  ? "nav-item mr-2  mini-item active"
-                  : "nav-item mr-2  mini-item"
-              }
+                className={
+                  props.header === 2
+                    ? "nav-item mr-2 active"
+                    : "nav-item mr-2"
+                }
+              >
+                <NavLink className="nav-link" to="/store" exact>
+                  Sản phẩm
+                </NavLink>
+              </li>
+              <li
+                className={
+                  props.header === 3
+                    ? "nav-item mr-2 active"
+                    : "nav-item mr-2"
+                }
+              >
+                <NavLink className="nav-link" to="/cart" exact>
+                  Giỏ hàng
+                </NavLink>
+              </li>
+              {props.user && (
+                <li
+                  className={
+                    props.header === 5
+                      ? "nav-item mr-2 active"
+                      : "nav-item mr-2"
+                  }
+                >
+                  <NavLink className="nav-link" to="/order" exact>
+                    Đơn hàng
+                  </NavLink>
+                </li>
+              )}
+              <li
+                className={
+                  props.header === 4
+                    ? "nav-item mr-2 active"
+                    : "nav-item mr-2"
+                }
+              >
+                <NavLink className="nav-link" to="/blog" exact>
+                  Chính sách
+                </NavLink>
+              </li>
+              {props.user && (
+                <li
+                  className={
+                    props.header === 6
+                      ? "nav-item mr-2 active"
+                      : "nav-item mr-2"
+                  }
+                >
+                  <NavLink className="nav-link" to="/chat" exact>
+                    Hỏi đáp
+                  </NavLink>
+                </li>
+              )}
+            </ul>
+            <form
+              className="form-inline my-2 my-lg-0 mr-3 search-bar"
+              onSubmit={(e) => submitHandler(e)}
             >
-              <NavLink className="nav-link" to="/chat" exact>
-                Hỏi đáp
-              </NavLink>
-            </li>
-            )}
-          </ul>
-          <form
-            className="form-inline my-2 my-lg-0 mr-3"
-            onSubmit={(e) => submitHandler(e)}
-          >
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              aria-label="Search"
-              name="keyword"
-            />
-            <button>
-              <i
-                className="fa fa-search ml-1"
-                aria-hidden="true"
-                style={{ fontSize: "24px" }}
-              ></i>
-            </button>
-          </form>
-          {/* {props.user && (
-            <Dropdown
+              <input
+                className="form-control mr-sm-2"
+                style={{borderRadius: "50px"}}
+                type="search"
+                aria-label="Search"
+                name="keyword"
+              />
+              <button className={"btn btn-secondary search-btn"} style={{borderRadius: "50px"}}>
+                <i
+                  className={"fa fa-search"}
+                  aria-hidden="true"
+                  style={{fontSize: "24px"}}
+                ></i>
+              </button>
+            </form>
+            {/* {props.user && (
+    <Dropdown
+      customToggle={() => renderUserToggle(curr_user)}
+      contentData={user_menu}
+      renderItems={(item, index) => renderUserMenu(item, index)}
+    />
+    )}
+    {!props.user && (
+    <Dropdown
+      customToggle={() => renderUserToggle(curr_user)}
+      contentData={not_menu}
+      renderItems={(item, index) => renderUserMenu(item, index)}
+    />
+    )} */}
+            {props.user ? <Dropdown
               customToggle={() => renderUserToggle(curr_user)}
               contentData={user_menu}
               renderItems={(item, index) => renderUserMenu(item, index)}
-            />
-          )}
-          {!props.user && (
-            <Dropdown
-              customToggle={() => renderUserToggle(curr_user)}
-              contentData={not_menu}
-              renderItems={(item, index) => renderUserMenu(item, index)}
-            />
-          )} */}
-          {props.user ?  <Dropdown
-              customToggle={() => renderUserToggle(curr_user)}
-              contentData={user_menu}
-              renderItems={(item, index) => renderUserMenu(item, index)}
-            /> :  <Dropdown
+            /> : <Dropdown
               customToggle={() => renderUserToggle(curr_user)}
               contentData={not_menu}
               renderItems={(item, index) => renderUserMenu(item, index)}
             />}
-        </div>
-      </nav>
-    </div>
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
